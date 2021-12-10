afeb78e02c52e4e9bc6f980965e833a3f0bc2450
chenxuewei <chen.xuewei@yilutong.com>
2021-12-10 09:09:08 +0800

    暂存

--- 转盘/src/ES6/turntable.js
+++ 转盘/src/ES6/turntable.js
@@ -1,7 +1,8 @@
 
-class Turntable extends Basic {
+class Turntable extends CanvasAdaptation {
     constructor() {
         super()
+        this.initCanvas({designWidth:100,designHeight:200,el:document.getElementById('test')})
     }
 }
 
